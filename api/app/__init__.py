85d78481287d591a3d3c10b1913a3fffa12f0438
Thomas Moellers <thomas.moellers@rwth-aachen.de>
2020-08-28 07:54:17 +0200

    all the containers - not working

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,8 +1,10 @@
 # following http://www.patricksoftwareblog.com/structuring-a-flask-project/
-from app.extensions import db, migrate, bcrypt, mail, cors, limiter
+from app.extensions import (db, migrate, bcrypt, mail, cors, limiter, cache, session, socket_io)
 #from app.logs.service import LogService
 from flask import Flask, jsonify
 from werkzeug.contrib.fixers import ProxyFix
+from dynaconf.contrib import FlaskDynaconf
+from celery import Celery
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,6 +15,10 @@ def create_app(env):
     app.config.from_object(app_config[env])
     # for example if the request goes through one proxy before hitting your application server
     app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=1)
+
+    # make app.config support dot notation.
+    FlaskDynaconf(app=app)
+
     register_extensions(app)
     register_api(app)
     # !!! LogService.setup_logging(app)
@@ -28,12 +34,11 @@ def create_app(env):
         if exception:
             db.session.rollback()
         db.session.remove()
-        print('RUN ON TEARDOWN', flush=True)
 
     return app
 
 
-def register_extensions(app):
+def register_extensions(app) -> None:
     # Since the application instance is now created, pass it to each Flask
     # extension instance to bind it to the Flask application instance (app)
     db.init_app(app)
@@ -42,6 +47,21 @@ def register_extensions(app):
     cors.init_app(app)
     mail.init_app(app)
     limiter.init_app(app)
+    session.init_app(app)
+
+    # init Cache
+    cache.init_app(app, {
+        "CACHE_TYPE": "redis",
+        "CACHE_REDIS_URL": app.config.CACHE_REDIS_URL
+    })
+
+    # init SocketIO
+    if not app.config.CELERY_BROKER_URL:
+        logging.warn(
+            """app.config.CELERY_BROKER_URL is not set. """
+            """SocketIO may not work with Celery workers now.""")
+
+    socket_io.init_app(app=app, message_queue=app.config.CELERY_BROKER_URL)
 
     return None
 
@@ -53,3 +73,24 @@ def register_api(app):
     register_routes(api, app)
     #register_routes(api, app, root=os.getenv('API_ROOT'))
     return None
+
+
+def create_celery(app):
+    """
+    Initializes a celery application using Flask App
+        (retrieved from `http://flask.pocoo.org/docs/1.0/patterns/celery/`).
+    """
+    celery = Celery(app.import_name,
+                    broker=app.config.CELERY_BROKER_URL,
+                    backend=app.config.CELERY_RESULT_BACKEND)
+    # celery.conf.update(app.config)
+    celery.conf.CELERY_IMPORTS = app.config.CELERY_IMPORTS
+    celery.conf.BROKER_HEARTBEAT = app.config.CELERY_BROKER_HEARTBEAT
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
--- api/app/extensions/__init__.py
+++ api/app/extensions/__init__.py
@@ -1,10 +1,14 @@
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+from flask_caching import Cache
 from flask_mail import Mail
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+
+from .socketio import socket_io
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -14,4 +18,7 @@ mail = Mail()
 limiter = Limiter(
     key_func=get_remote_address,
     default_limits=["10 per minute", "5 per second"]
-    )
+)
+
+session = Session()
+cache = Cache()
