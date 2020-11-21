254311bd4ac302811a969a328373596c78094e99
Thomas Moellers <thomas.moellers@rwth-aachen.de>
2020-11-21 19:40:59 +0100

    sample card custom design test

---
+++ front/components/Cards/CardsSample/CardSampleTest.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card-sample">
+    <b-row>
+      <b-col cols="5">
+        <b-img src="@/assets/img/illustrations/data_upload.svg" fluid />
+      </b-col>
+      <b-col cols="7">
+        <h6 class="text-primary">
+          Sample Company
+        </h6>
+        <h4>Bond Store Ltd</h4>
+        <p class="my-2 text-dark">
+          The sample company at hand is the 'Bond Store Ltd' based in London, GB.
+        </p>
+      </b-col>
+    </b-row>
+    <p class="text-muted text-right">
+      1/XX
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardSampleTest",
+}
+</script>
+
+<style>
+    .card-sample {
+        background-color: rgb(166, 241, 241);
+        padding-bottom: 1rem;
+        padding-left: 3rem;
+        padding-right: 3rem;
+        padding-top: 3rem;
+        border-radius: 15px;
+        min-width: 30rem;
+        max-width: 50rem;
+        box-shadow: 6px 6px 12px #bdbcbc, -6px -6px 12px #ffffff !important;
+    }
+</style>
---
+++ front/components/Sections/VatCompliance/SectionVatComplianceDemoIntroduction.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <b-row>
+      <b-col>
+        <b-card
+          bg-variant="info"
+          text-variant="white"
+        >
+          <b-card-text>
+            We have prepared a small demo that will provide you
+            a good idea how this process works.
+          </b-card-text>
+        </b-card>
+        <card-sample-test class="mt-5" />
+      </b-col>
+      <b-col />
+    </b-row>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SectionVatComplianceDemoIntroduction'
+
+}
+</script>
+
+<style>
+
+</style>
